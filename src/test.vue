fa19a679dcb905b58f082b97d02d85c23da8bca3
na-luo <942210530@qq.com>
2022-11-07 20:13:04 +0800

    git commit

---
+++ src/test.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        
+    </div>
+</template>
+
+<script lang = "ts">
+    import x from '@/components/test.vue'
+    
+    
+</script>
+
+<style lang="scss" scoped>
+
+</style>
